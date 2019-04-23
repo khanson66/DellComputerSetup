c4c325c377925e5a8c7a9c6ef517ebcbaf16f964
khanson99944 <kh99944p@pace.edu>
2019-04-23 18:20:43 -0400

    updated ninite URL

--- ConfigVar.ps1
+++ ConfigVar.ps1
@@ -1,4 +1,4 @@
 #url path to the exe for the ninite install file with the feature that I thought was important
-$NiniteURL = 'https://ninite.com/.net4.7.2-7zip-air-chrome-firefox-java8-shockwave-silverlight-vlc/ninite.exe'
+$NiniteURL = 'https://ninite.com/.net4.7.2-7zip-adoptjava8-adoptjavax11-adoptjavax8-air-chrome-firefox-silverlight-vlc/ninite.exe'
 #url to the exe installer
 $dellCommandURL = 'https://downloads.dell.com/FOLDER05055451M/1/Dell-Command-Update_DDVDP_WIN_2.4.0_A00.EXE'
